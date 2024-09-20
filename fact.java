81ac63e2bcb2d6e348d4e8a7bfce5ddc9f8a17e7
Atul Gupta <atulajitdewantigupta@gmail.com>
2024-09-20 22:13:46 +0530

    first commit

---
+++ fact.java
@@ -0,0 +1,10 @@
+public class fact{  
+    public static void main(String args[]){  
+     int i,fac=1;  
+     int number=10;    
+     for(i=1;i<=number;i++){    
+         fac=fac*i;    
+     }    
+     System.out.println("Factorial of "+number+" is:- "+fac);    
+    }  
+   } 
